39753669c9d60f0177e6945e55fe847e7861cca7
furkan <furkanilgin@gmail.com>
2013-09-16 01:11:06 +0300

    tutorial 3

---
+++ src/javabrains/test/HibernateTest.java
@@ -0,0 +1,27 @@
+package javabrains.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import javabrains.tutorial_3_2.UserDetails;
+
+public class HibernateTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		UserDetails user = new UserDetails();
+		user.setUserId(1);
+		user.setUserName("First User");
+		
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+	}
+
+}
