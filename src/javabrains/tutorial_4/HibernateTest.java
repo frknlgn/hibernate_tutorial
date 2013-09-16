7c9e6acaac86a54adce36a77ac727f5f738d1800
furkan <furkanilgin@gmail.com>
2013-09-16 20:44:16 +0300

    tutorial 4 Column name annotation, annotion on the top of getter

---
+++ src/javabrains/tutorial_4/HibernateTest.java
@@ -0,0 +1,26 @@
+package javabrains.tutorial_4;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class HibernateTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		UserDetails user = new UserDetails();
+		user.setUserId(1);
+		user.setUserName("First User");
+		
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+	}
+
+}
---
+++ src/javabrains/tutorial_4/UserDetails.java
@@ -0,0 +1,29 @@
+package javabrains.tutorial_4;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity (name = "USER_DETAILS")
+public class UserDetails {
+
+	@Id
+	@Column (name = "USER_ID")
+	private int userId;
+	private String userName;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	@Column (name = "USER_NAME")
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
