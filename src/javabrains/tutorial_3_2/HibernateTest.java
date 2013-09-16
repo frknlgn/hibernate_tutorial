34dbb899265121eac6cd24ef1966a4e023af5105
furkan <furkanilgin@gmail.com>
2013-09-16 20:11:05 +0300

    directory arrangement

--- src/javabrains/tutorial_3_2/HibernateTest.java
+++ src/javabrains/tutorial_3_2/HibernateTest.java
@@ -1,10 +1,9 @@
-package javabrains.test;
+package javabrains.tutorial_3_2;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import javabrains.tutorial_3_2.UserDetails;
 
 public class HibernateTest {
 
