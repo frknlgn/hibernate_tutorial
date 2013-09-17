57841db7b28425e1d84ea0ca893bbd662e658b1c
furkan <furkanilgin@gmail.com>
2013-09-18 00:26:47 +0300

    Tutorial 12 Proxy Objects and Eager and Lazy Fetch Types

---
+++ src/javabrains/tutorial_12/HibernateTest.java
@@ -0,0 +1,51 @@
+package javabrains.tutorial_12;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class HibernateTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		UserDetails user = new UserDetails();
+		user.setUserName("First User");
+		
+		Address addr = new Address();
+		addr.setStreet("First Street");
+		addr.setCity("First City");
+		addr.setState("First State");
+		addr.setPincode("10001");
+		
+		Address addr2 = new Address();
+		addr2.setStreet("Second Street");
+		addr2.setCity("Second City");
+		addr2.setState("Second State");
+		addr2.setPincode("20002");
+		
+		user.getListOfAddresses().add(addr);
+		user.getListOfAddresses().add(addr2);
+		
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+		session.close();
+	
+		user = null;
+		
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		user = (UserDetails) session.get(UserDetails.class, 1);
+		session.close();
+		System.out.println(user.getListOfAddresses().size()); // can be lazy or eager
+	}
+
+}
