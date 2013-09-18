038cbd60aa3fe653813db333c4d08b4d7e7620d0
furkan <furkanilgin@gmail.com>
2013-09-19 00:12:07 +0300

    tutorial 15 mappedBy and Many To Many Mapping

---
+++ src/javabrains/tutorial_15/HibernateTest.java
@@ -0,0 +1,41 @@
+package javabrains.tutorial_15;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class HibernateTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		UserDetails user = new UserDetails();
+		user.setUserName("First User");
+
+		Vehicle vehicle = new Vehicle();
+		vehicle.setVehicleName("Car");
+		vehicle.getUserList().add(user);
+		
+		Vehicle vehicle2 = new Vehicle();
+		vehicle2.setVehicleName("Jeep");
+		vehicle2.getUserList().add(user);
+		
+		user.getVehicle().add(vehicle);
+		user.getVehicle().add(vehicle2);
+		
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.save(vehicle);
+		session.save(vehicle2);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+}
